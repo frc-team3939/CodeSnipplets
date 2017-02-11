1707eb58c672e907ddeb3815896e2caa2f02d988
Anthony <tonycub3@gmail.com>
2017-02-11 15:48:50 -0500

    No more errors :)

--- CodeSnipplets/src/org/usfirst/frc/team3939/robot/Drivetoadistance.java
+++ CodeSnipplets/src/org/usfirst/frc/team3939/robot/Drivetoadistance.java
@@ -25,15 +25,15 @@ public class Drivetoadistance extends IterativeRobot {
 	RobotDrive tankDrive = new RobotDrive(0, 1);
 	Encoder encoder = new Encoder(0, 1, true, EncodingType.k4X);
 
-	public static final double WHEEL_DIAMETER = 4;
+	public static final double WHEEL_DIAMETER = 4; //Will need to be set before use
 	public static final double PULSE_PER_REVOLUTION = 360;
 	public static final double ENCODER_GEAR_RATIO = 0;
 	public static final double GEAR_RATIO = 8.45 / 1;
 	public static final double FUDGE_FACTOR = 1.0;
 
         public void autonomousInit() {	
-                final double distancePerPulse = Math.PI * Defines.WHEEL_DIAMETER / Defines.PULSE_PER_REVOLUTION
-				/ Defines.ENCODER_GEAR_RATIO / Defines.GEAR_RATIO * Defines.FUDGE_FACTOR;
+                final double distancePerPulse = Math.PI * WHEEL_DIAMETER / PULSE_PER_REVOLUTION
+				/ ENCODER_GEAR_RATIO / GEAR_RATIO * FUDGE_FACTOR;
 	        encoder.setDistancePerPulse(distancePerPulse);
 
 	}
